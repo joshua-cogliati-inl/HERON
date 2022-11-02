db197d80396fe471aa204a05d75e9a681ca26bfa
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2022-11-02 14:21:24 -0600

    Fix path.

--- src/Hybrid2Heron/hybrid2heron_economic.py
+++ src/Hybrid2Heron/hybrid2heron_economic.py
@@ -34,7 +34,8 @@ try:
   HERON_src_path = HERON.src.__path__[0]
 except ModuleNotFoundError:
   # # Importing XML utility from RAVEN to make the output XML file looks pretty
-  HERON_src_path = os.path.dirname(os.path.abspath(__file__)).split("HERON")[0] + "HERON/src"
+  HERON_src_path = os.path.join(os.path.dirname(os.path.abspath(__file__),"..","..","src")
+  print("file and path", __file__, HERON_src_path)
   sys.path.append(HERON_src_path)
   from _utils import get_raven_loc
   sys.path.append(get_raven_loc())
