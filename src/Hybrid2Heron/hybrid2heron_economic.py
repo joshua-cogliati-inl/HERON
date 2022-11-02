8a9cadb852d3c7bb1d0592f85c1048b7fa576f0b
Joshua J. Cogliati <joshua.cogliati@inl.gov>
2022-11-02 15:40:04 -0600

    Alternative way to split path.

--- src/Hybrid2Heron/hybrid2heron_economic.py
+++ src/Hybrid2Heron/hybrid2heron_economic.py
@@ -34,7 +34,7 @@ try:
   HERON_src_path = HERON.src.__path__[0]
 except ModuleNotFoundError:
   # # Importing XML utility from RAVEN to make the output XML file looks pretty
-  HERON_src_path = os.path.join(os.path.dirname(os.path.abspath(__file__),"..","..","src")
+  HERON_src_path = "HERON".join(__file__.split("HERON")[:-1])+"HERON/src"
   print("file and path", __file__, HERON_src_path)
   sys.path.append(HERON_src_path)
   from _utils import get_raven_loc
